bc74d53cf85a7b1d42530c72d367a5116875781d
German <chendillo@gmail.com>
2014-08-04 10:45:57 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GurbinaSecondsample::Application.config.secret_token = '8a79d1e741105e93ca1ee10ca74dfe5ab21e5760bbb00821527b4c0f8cd0b2d780460aafbcd7ff573fbf54d871bc1f0536968759e7dcce032184fd62774ada66'
